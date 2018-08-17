cae5c6290459504999e5380998db07faa79e76b2
siddhahast mohapatra <siddhahast.mohapatra@1stdibs.com>
2018-08-16 23:01:19 -0400

    math egyptian fraction

--- src/Greedy/EgyptianFractions.java
+++ src/Greedy/EgyptianFractions.java
@@ -3,6 +3,41 @@ package Greedy;
 public class EgyptianFractions
 {
 
+	private static void printEgyptian(int nr, int dr)
+	{
+		if(nr==0 || dr==0)
+		{
+			return;
+		}
+		
+		if(dr%nr == 0)
+		{
+			System.out.print("1/"+(dr/nr)+" ");
+			return;
+		}
+		
+		if(nr%dr == 0)
+		{
+			System.out.print(nr/dr);
+			return;
+		}
+		
+		if(nr > dr)
+		{
+			System.out.print(nr/dr + " + ");
+			printEgyptian(nr%dr, dr);
+			return;
+		}
+		
+		int ceiling = dr/nr + 1;
+		System.out.print("1/"+ceiling + " + ");
+		printEgyptian(nr*ceiling - dr, dr*ceiling);
+	}
 	
-	
+	public static void main(String[] args)
+	{
+		int nr = 7;
+		int dr = 3;
+		printEgyptian(nr, dr);
+	}
 }
